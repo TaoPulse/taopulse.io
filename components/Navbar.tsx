542944492c616e5710f88407b1638a646cf61ec9
TaoPulse <hello@taopulse.io>
2026-03-25 10:41:30 -0400

    Replace raster logo with SVG for crisp rendering at all sizes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -130,7 +130,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 shrink-0">
             <Image
-              src="/logo-clean.png"
+              src="/logo.svg"
               alt="TaoPulse logo"
               width={32}
               height={32}
