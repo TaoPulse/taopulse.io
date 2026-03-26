0c0de7d1b43933b3ef6eaf40a440bc61ede8e840
TaoPulse <hello@taopulse.io>
2026-03-26 12:38:27 -0400

    fix: use www.taopulse.io in sitemap URLs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://taopulse.io";
+  const baseUrl = "https://www.taopulse.io";
 
   return [
     {
